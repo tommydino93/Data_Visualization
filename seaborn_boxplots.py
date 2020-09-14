768b15398080adaeb87da48446be08356c0b27fb
Tommaso Di Noto <tommydino@hotmail.it>
2020-09-14 16:47:08 +0200

    Updated example

--- seaborn_boxplots.py
+++ seaborn_boxplots.py
@@ -5,25 +5,26 @@ import matplotlib.pyplot as plt
 
 np.random.seed(123)  # set random seed for reproducibility
 
-weight_subject_1 = np.random.uniform(low=50, high=90, size=10)  # kg
-weight_subject_2 = np.random.uniform(low=50, high=90, size=10)  # kg
-weight_subject_3 = np.random.uniform(low=50, high=90, size=10)  # kg
+# we have three groups of individuals and we want to compare their weights
+
+weights_group_1 = np.random.uniform(low=50, high=90, size=10)  # kg
+weights_group_2 = np.random.uniform(low=50, high=90, size=10)  # kg
+weights_group_3 = np.random.uniform(low=50, high=90, size=10)  # kg
 
 # convert lists to dictionaries
-d_weight_1 = {"weight_1": weight_subject_1}
-d_weight_2 = {"weight_2": weight_subject_2}
-d_weight_3 = {"weight_3": weight_subject_3}
+d_weights_1 = {"weights_1": weights_group_1}
+d_weights_2 = {"weights_2": weights_group_2}
+d_weights_3 = {"weights_3": weights_group_3}
 
 # convert dictionaries to pandas Dataframe
-df_weight_1 = pd.DataFrame(d_weight_1)
-df_weight_2 = pd.DataFrame(d_weight_2)
-df_weight_3 = pd.DataFrame(d_weight_3)
+df_weights_1 = pd.DataFrame(d_weights_1)
+df_weights_2 = pd.DataFrame(d_weights_2)
+df_weights_3 = pd.DataFrame(d_weights_3)
 
 # concatenate Dataframes into one unique Dataframe
-merged_df = pd.concat([df_weight_1, df_weight_2, df_weight_3], ignore_index=True, axis=1)
-merged_df.columns = ["sub_1", "sub_2", "sub_3"]  # assign names to columns
+merged_df = pd.concat([df_weights_1, df_weights_2, df_weights_3], ignore_index=True, axis=1)
+merged_df.columns = ["group_1", "group_2", "group_3"]  # assign names to columns
 
-# create boxplots
 _ = sns.boxplot(data=merged_df)
 # set ylabel
 plt.ylabel("[kg]", fontsize=12)
