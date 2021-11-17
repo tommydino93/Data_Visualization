6eee21c566659a516d979e515746f79d370b9f9a
Tommaso Di Noto <tommydino@hotmail.it>
2021-11-17 15:05:38 +0100

    create seaborn_histogram.py

---
+++ seaborn_histogram.py
@@ -0,0 +1,21 @@
+import random
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# This script plots a histogtam of randomly generated ages with seaborn
+
+random_ages = []  # create empty list
+
+# generate 200 invented ages of people
+for i in range(200):
+  random_ages.append(random.randint(0,100))  # assume the age is between 0 and 100 years
+
+# plot histogram of ages
+ax = sns.histplot(random_ages)
+# set xlabel
+ax.set_xlabel("count", fontsize=12)
+# set ylabel
+ax.set_ylabel("age", fontsize=12)
+# set bold title
+ax.set_title("Age histogram", weight="bold", fontsize=15)
+plt.show()
